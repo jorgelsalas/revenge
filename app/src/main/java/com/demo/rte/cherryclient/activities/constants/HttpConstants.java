e808a8124925495100fa6ad7c87de84eb44d6d0c
Jorge Salas <jorgesalch@yahoo.com>
2015-08-04 22:10:44 -0600

    changed to new server address

--- app/src/main/java/com/demo/rte/cherryclient/activities/constants/HttpConstants.java
+++ app/src/main/java/com/demo/rte/cherryclient/activities/constants/HttpConstants.java
@@ -5,7 +5,7 @@ package com.demo.rte.cherryclient.activities.constants;
  */
 public class HttpConstants {
 
-    public static final String ENDPOINT = "http://javalibtest.mybluemix.net/api/";
+    public static final String ENDPOINT = "http://cherrytest.mybluemix.net/api/";
     public static final String NAME_TAG = "name";
     public static final String PACKAGE_PATH = "packages";
     public static final String ACRONYM_TAG = "acronym";
